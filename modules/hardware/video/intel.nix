a4ee027e513fd129f51f731f6c424fbc69cf5469
0xB4DB47 <lpenapenn@gmail.com>
2026-04-29 12:36:39 -0400

    commenting out the changes to prevent hibernating - no longer hosting windrose server

--- modules/hardware/video/intel.nix
+++ modules/hardware/video/intel.nix
@@ -14,7 +14,7 @@
     "i915.enable_psr=1" # Panel Self Refresh for power savings
     "i915.enable_fbc=1" # Framebuffer compression
     "i915.fastboot=1" # Skip unnecessary mode sets at boot
-    # "mem_sleep_default=deep" # Allow deepest sleep states - disabled for server use
+    "mem_sleep_default=deep" # Allow deepest sleep states - disabled for server use
     "i915.enable_dc=2" # Display power saving
     "nvme.noacpi=1" # Helps with NVME power consumption
   ];
